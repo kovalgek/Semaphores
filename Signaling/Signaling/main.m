b888631018265b9f151cdd5c278dca3b1580a01e
Anton Kovalchuk <koval400k@gmail.com>
2019-03-14 23:15:02 +0100

    signaling

---
+++ Signaling/Signaling/main.m
@@ -0,0 +1,31 @@
+//
+//  main.m
+//  Signaling
+//
+//  Created by Anton Kovalchuk on 04.03.2019.
+//  Copyright © 2019 Anton Kovalchuk. All rights reserved.
+//
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool
+    {
+        dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+
+        // Thread A
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            NSLog(@"a1 thread=%@", [NSThread currentThread]);
+            dispatch_semaphore_signal(semaphore);
+        });
+        
+        // Thread B
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER);
+            NSLog(@"b1 thread=%@", [NSThread currentThread]);
+        });
+        
+        [[NSRunLoop currentRunLoop] run];
+    }
+    return 0;
+}
